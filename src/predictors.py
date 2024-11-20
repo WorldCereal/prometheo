57ef012a642b4e2f26238b5d806db1d8a7a64be1
Christina Butsko <cbutsko@cbutsko.vitovm.rscloud.vito.be>
2024-11-20 14:54:29 +0100

    let's relaunch

---
+++ src/predictors.py
@@ -0,0 +1,15 @@
+import numpy as np
+from Typing import NamedTuple, Optional, Union
+
+NODATAVALUE = 65535
+
+
+class Predictors(NamedTuple):
+    s1: Optional[np.ndarray] = None
+    s2: Optional[np.ndarray] = None
+    meteo: Optional[np.ndarray] = None
+    dem: Optional[np.ndarray] = None
+    latlon: Optional[np.ndarray] = None
+    valid_date: Optional[np.ndarray] = None
+    label: Optional[Union[np.ndarray,float]] = None
+    month: Optional[Union[np.ndarray,int]] = None
