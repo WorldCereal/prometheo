b1f1e53246774d6fecac964e4bc72bd029dbcea7
Christina Butsko <cbutsko@cbutsko.vitovm.rscloud.vito.be>
2024-11-25 10:31:44 +0100

    latest changes from the in-person brainstorm session

--- src/predictors.py
+++ src/predictors.py
@@ -1,15 +1,24 @@
 import numpy as np
-from Typing import NamedTuple, Optional, Union
+from Typing import List, NamedTuple, Optional, Union
 
 NODATAVALUE = 65535
 
 
+# NEED THOROUGH AND PRECISE DOCUMENTATION AND TESTS!!!
+# checks should be implemented at the test level
+# here, we need to fix the exted ranges for the bands
+S1_bands = [("VV", "db"),()]
+meteo_bands = []
+
+
 class Predictors(NamedTuple):
-    s1: Optional[np.ndarray] = None
+    s1: Optional[np.ndarray] = None  # VV VH 
     s2: Optional[np.ndarray] = None
     meteo: Optional[np.ndarray] = None
     dem: Optional[np.ndarray] = None
     latlon: Optional[np.ndarray] = None
-    valid_date: Optional[np.ndarray] = None
-    label: Optional[Union[np.ndarray,float]] = None
+    # Gabi to try and implement the possibility to learn a linear layer for each aux_input
+    aux_inputs: Optional[List[np.ndarray]] = None
+    # Label needs to always be 2D, with temporal dimension
+    label: Optional[np.ndarray] = None
     month: Optional[Union[np.ndarray,int]] = None
