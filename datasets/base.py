1f905d7cef907b175aa513425f83a9e1e1e53390
Giorgia Milli <giorgia.milli@vito.be>
2024-11-28 10:36:16 +0100

    init updates

--- datasets/base.py
+++ datasets/base.py
@@ -1,13 +1,22 @@
+from typing import Literal
+
 import pandas as pd
 from torch.utils.data import Dataset
 
 
 class DatasetBase(Dataset):
-    
-    def __init__(self, dataframe: pd.DataFrame):
+
+    def __init__(
+        self,
+        dataframe: pd.DataFrame,
+        num_outputs: int,
+        num_timesteps: int,
+        task_type: Literal["binary", "multiclass", "regression"],
+    ):
         self.df = dataframe
-        self.num_outputs: int
-        self.task_type: str
+        self.num_outputs = num_outputs
+        self.num_timesteps = num_timesteps
+        self.task_type = task_type
 
         assert self.task_type in ["binary", "multiclass", "regression"]
 
