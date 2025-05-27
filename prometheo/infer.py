560e3d031ac82a2c84047a78e2c920af1187a73e
Kristof Van Tricht <kristof.vantricht@vito.be>
2025-05-27 14:56:50 +0200

    Added docstring

--- prometheo/infer.py
+++ prometheo/infer.py
@@ -1,7 +1,8 @@
 import torch
 from torch import nn
-from .predictors import Predictors
+
 from .models.pooling import PoolingMethods
+from .predictors import Predictors
 
 
 def extract_features_from_model(
@@ -10,6 +11,25 @@ def extract_features_from_model(
     batch_size: int,
     pooling: PoolingMethods,
 ) -> torch.Tensor:
+    """
+    Extract features from a model for the given input data using batching and pooling.
+
+    Parameters
+    ----------
+    model : nn.Module
+        The model (wrapper) from which to extract features.
+    x : Predictors
+        The input data wrapped in a Predictors object.
+    batch_size : int
+        The number of samples per batch.
+    pooling : PoolingMethods
+        The pooling method to apply to the model outputs.
+
+    Returns
+    -------
+    torch.Tensor
+        A tensor containing the concatenated features extracted from the model for all input data.
+    """
     model_features: list[torch.Tensor] = []
 
     # Ensure model is in evaluation mode
